f3082880d1472d3dcc0c0c72f57f888e85ede915
Michael Vaisnoras <mvaisnoras01@qub.ac.uk>
2025-02-01 18:00:17 +0000

    finished to-do parts 1 - 5 excl part 3 and added more

---
+++ Lab1/Lab1Code/src/tests/AccountTest.java
@@ -0,0 +1,67 @@
+package tests;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import model.Account;
+
+public class AccountTest {
+	
+	// test message text colours
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	
+	public static void main(String[] args) {
+		
+		testAccountConstructor();
+		
+	}
+	
+	public static void testAccountConstructor() {
+		// 1 Setup
+		String test_account_number = "123456789";
+		String test_username_of_account_holder = "mike@mail.com";
+		String test_account_type = "Savings";
+		Date test_account_opening_date = new Date();
+		
+		// 2 Construct the object using setup data
+		Account testAccount = new Account(test_account_number, test_username_of_account_holder, 
+				test_account_type, test_account_opening_date);
+			
+		// 3 Verify (Assert)
+		System.out.println("Starting the assertions of the test method: testAccountConstructor");
+		
+		if (testAccount.getAccount_number() == test_account_number) {
+			System.out.println(ANSI_GREEN + "TC1-getAccount_number-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC1-getAccount_number-FAILED" + ANSI_RESET);
+		}
+		
+		if (testAccount.getUsername_of_account_holder() == test_username_of_account_holder) {
+			System.out.println(ANSI_GREEN + "TC2-getUsername_of_account_holder-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC2-getUsername_of_account_holder-FAILED"+ ANSI_RESET);
+		}
+		
+		if (testAccount.getAccount_type() == test_account_type) {
+			System.out.println(ANSI_GREEN + "TC3-getAccount_type-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC3-getAccount_type-FAILED"+ ANSI_RESET);
+		}
+		
+		if (testAccount.getAccount_opening_date() == test_account_opening_date) {
+			System.out.println(ANSI_GREEN + "TC4-getAccount_opening_date-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC4-getAccount_opening_date-FAILED"+ ANSI_RESET);
+		}
+		
+		assert testAccount.getAccount_number() == test_account_number;
+		assert testAccount.getUsername_of_account_holder() == test_username_of_account_holder;
+		assert testAccount.getAccount_type() == test_account_type;
+		assert testAccount.getAccount_opening_date() == test_account_opening_date;
+		
+		System.out.println("All java assertions in the Account Constructor test suite passed.");
+	}	
+	
+}
--- Lab1/Lab1Code/src/tests/UserTest.java
+++ Lab1/Lab1Code/src/tests/UserTest.java
@@ -2,14 +2,76 @@ package tests;
 
 import model.User;
 
-//Manual Testing of the User Class
+//Testing of the User Class
 public class UserTest {
 
+	// test message text colours
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+
 	public static void main(String[] args) {
-		User testUser = new User("mike@mail.com", "password123", "Mike", "Smith", "12345");
 		
+		/* OLD manual testing.
+		User testUser = new User("mike@mail.com", "password123", "Mike", "Smith", "12345");
 		System.out.println(testUser);
-
+		*/
+		
+		testUserConstructor();
+		
 	}
-
+	
+	public static void testUserConstructor() {
+		// 1 Setup
+		String test_user = "mike@mail.com";
+		String test_pass = "password123";
+		String test_first = "Mike";
+		String test_last = "Smith";
+		String test_mobile = "123456789";
+		
+		// 2 Construct the object using setup data
+		User testUser = new User(test_user, test_pass, test_first, test_last, test_mobile);
+			
+		// 3 Verify (Assert)
+		System.out.println("Starting the assertions of the test method: testUserConstructor");
+		
+		if (testUser.getUsername() == test_user) {
+			System.out.println(ANSI_GREEN + "TC1-getUsername-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC1-getUsername-FAILED" + ANSI_RESET);
+		}
+		
+		if (testUser.getPassword() == test_pass) {
+			System.out.println(ANSI_GREEN + "TC2-getPassword-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC2-getPassword-FAILED"+ ANSI_RESET);
+		}
+		
+		if (testUser.getFirst_name() == test_first) {
+			System.out.println(ANSI_GREEN + "TC3-getFirst_name-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC3-getFirst_name-FAILED"+ ANSI_RESET);
+		}
+		
+		if (testUser.getLast_name() == test_last) {
+			System.out.println(ANSI_GREEN + "TC4-getLast_name-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC4-getLast_name-FAILED"+ ANSI_RESET);
+		}
+		
+		if (testUser.getMobile_number() == test_mobile) {
+			System.out.println(ANSI_GREEN + "TC5-getMobile_number-Passed"+ ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "TC5-getMobile_number-FAILED"+ ANSI_RESET);
+		}
+		
+		assert testUser.getUsername() == test_user;
+		assert testUser.getPassword() == test_pass;
+		assert testUser.getFirst_name() == test_first;
+		assert testUser.getLast_name() == test_last;
+		assert testUser.getMobile_number() == test_mobile;
+		
+		System.out.println("All java assertions in the User Constructor test suite passed.");
+	}	
 }
