9e075059cb6bde61154c7df20121ddb9828a2f59
deanshine743 <131590033+deanshine743@users.noreply.github.com>
2025-02-19 19:38:05 +0000

    Update TestUtils.java

--- Lab1/Lab1Code/src/utility/TestUtils.java
+++ Lab1/Lab1Code/src/utility/TestUtils.java
@@ -1,8 +1,22 @@
 package utility;
 
 public class TestUtils {
-	// test message text colours
-	public static final String TEXT_COLOR_RESET = "\u001B[0m";
-	public static final String TEXT_COLOR_GREEN = "\u001B[32m";
-	public static final String TEXT_COLOR_RED = "\u001B[31m";
+    // Test message text colors
+    public static final String TEXT_COLOR_RESET = "\u001B[0m";
+    public static final String TEXT_COLOR_GREEN = "\u001B[32m";
+    public static final String TEXT_COLOR_RED = "\u001B[31m";
+
+
+	//Prints a test pass message in green.
+    
+    public static void printTestPass(String testName) {
+        System.out.println(TEXT_COLOR_GREEN + testName + " - PASSED" + TEXT_COLOR_RESET);
+    }
+
+    
+      //Prints a test fail message in red.
+ 
+    public static void printTestFail(String testName) {
+        System.out.println(TEXT_COLOR_RED + testName + " - FAILED" + TEXT_COLOR_RESET);
+    }
 }
