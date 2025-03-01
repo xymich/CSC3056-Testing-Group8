3b82e18ec3faee37b1f69f239a5d9680b2177619
Michael Vaisnoras <mvaisnoras01@qub.ac.uk>
2025-03-01 17:34:43 +0000

    minor

---
+++ Lab2/Lab2Code/src/org/jfree/data/test/RangeTest.java
@@ -0,0 +1,30 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RangeTest {
+
+	private Range rangeObjectUnderTest;
+
+	@Before
+	public void setUp() throws Exception {
+		rangeObjectUnderTest = new Range(-1, 1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCentralValueShouldBeZero() {
+		assertEquals("The central value of -1 and 1 should be 0",
+					0, rangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+
+
+}
